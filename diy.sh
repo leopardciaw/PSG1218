a1cbdbf8b80febffda759f1c72e57bb173600dd2
“leopardciaw” <quanyongw@gmail.com>
2020-04-07 11:26:25 +0800

    update

--- diy.sh
+++ diy.sh
@@ -12,4 +12,4 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 sed -i 's/OpenWrt/Leopard build $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 
 # 添加新的主题包
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+# git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
