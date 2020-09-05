ddf27929f2b1d91ee6771f6278db2c562b5493f9
leopardciaw <47556424+leopardciaw@users.noreply.github.com>
2020-09-06 05:52:06 +0800

    Update diy.sh
    update

--- diy.sh
+++ diy.sh
@@ -9,13 +9,13 @@
 sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
 
 # Modify the version number
-# sed -i 's/OpenWrt/Leopard build $(date "+%Y.%m.%d") @ OpenWrt/g' package/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt/Leopard build $(date "+%Y.%m.%d") @ OpenWrt/g' package/default-settings/files/zzz-default-settings
 
 # 添加新的主题包
-# git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 
 # 去除默认主题
-# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 添加clash支持
 # git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash
@@ -28,4 +28,4 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 
 # 增加ssr
 # git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
+# git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
