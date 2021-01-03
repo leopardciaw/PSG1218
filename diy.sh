72e9570556066c8dc6c81a294756c1182f009f6a
leopardciaw <47556424+leopardciaw@users.noreply.github.com>
2021-01-03 10:14:13 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -17,15 +17,3 @@ sed -i 's/192.168.1.1/192.168.5.201/g' package/base-files/files/bin/config_gener
 # 去除默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
-# 添加clash支持
-# git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash
-
-# 添加v2ray
-# git clone https://github.com/kuoruan/luci-app-v2ray.git package/lean/luci-app-v2ray
-# rm rm -rf package/lean/v2ray
-# git clone https://github.com/KFERMercer/openwrt-v2ray.git package/lean/v2ray
-# git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash
-
-# 增加ssr
-# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-# git clone https://github.com/fw876/helloworld.git package/openwrt-packages/luci-app-ssr-plus
